4c6e69dddddb4c5c544947c99e436d8dcd977c4a
Parv Choudhary <parv.ece.20089@iiitbh.ac.in>
2024-06-09 18:25:38 +0530

    simple-react-app

---
+++ react-techystar-master/src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/parv-choudhary-9620b4230/"
+            target={"blank"}
+          >
+            Linkedin
+          </a>
+          <a
+            href="https://www.instagram.com/parv_choudhary_2001"
+            target={"blank"}
+          >
+            Instagram
+          </a>
+          <a href="https://github.com/parv-2001" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
